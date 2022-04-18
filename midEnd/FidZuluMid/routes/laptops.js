b29f072aaf19750439c17e79d708b8e10f7b3d0b
a708437 <a708437@A708437WIN>
2022-04-18 14:33:06 -0400

    Created routes books-dvds-food-laptops-toys.

--- midEnd/FidZuluMid/routes/laptops.js
+++ midEnd/FidZuluMid/routes/laptops.js
@@ -2,28 +2,27 @@ var express = require('express');
 const request = require('request');
 var router = express.Router();
 
-/* Calculate Bike Prices */
+/* Calculate Laptop Prices */
 router.get('/:location', function(req, res, next) {
 
     var location = req.params.location;
     let response;
 
-    request('http://localhost:3031/bikes/' + location, { json: true }, (err, res, body) => {
-        
+    request('localhost:3035/laptops/' + location, {json: true}, (err, res, body) => {
+
         if(err) {
             console.log(err);
         }
-
+        
         console.log(body);
         let statusCode = res.statusCode;
         response = {
             statusCode,
             body
-        };
+        }
     });
 
     res.send(response);
 });
-
 
 module.exports = router;
