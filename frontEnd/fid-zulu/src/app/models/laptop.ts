b4c783e7f1444a3d9b3fa1312125395d19af4d71
Micah Haycraft <micahjhaycraft@gmail.com>
2022-04-18 16:13:13 -0400

    added laptop component model, list, etc etc

---
+++ frontEnd/fid-zulu/src/app/models/laptop.ts
@@ -0,0 +1,19 @@
+export class Laptop {
+  constructor(
+    public product: string,
+    public brand: string,
+    public CPU: string,
+    public memory: string,
+    public price: number
+  ) {}
+}
+
+/* example
+  {
+    "product": "ThinkPad T430s",
+    "brand": "Lenovo",
+    "CPU": "core i5-3320",
+    "memory": "8GB",
+    "price": 325.09
+  }
+  */
