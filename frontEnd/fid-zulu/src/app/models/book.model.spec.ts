e2ae8279afd2eaea5002a0fb6c71f5153573a4a6
Ky <hamky89@gmail.com>
2022-04-18 16:12:34 -0400

    book model test fixed

--- frontEnd/fid-zulu/src/app/models/book.model.spec.ts
+++ frontEnd/fid-zulu/src/app/models/book.model.spec.ts
@@ -2,6 +2,6 @@ import { Book } from './book.model';
 
 describe('Book', () => {
   it('should create an instance', () => {
-    expect(new Book()).toBeTruthy();
+    expect(new Book("title", "author", 99.99, "isbn", "publisher")).toBeTruthy();
   });
 });
