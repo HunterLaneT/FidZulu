b2e655e4eb6d79af55e891e85248dd5c87d53c27
Micah Haycraft <micahjhaycraft@gmail.com>
2022-04-18 16:23:50 -0400

    updated failing component tests, all are passing.

--- frontEnd/fid-zulu/src/app/models/food.spec.ts
+++ frontEnd/fid-zulu/src/app/models/food.spec.ts
@@ -2,6 +2,6 @@ import { Food } from './food';
 
 describe('Food', () => {
   it('should create an instance', () => {
-    expect(new Food()).toBeTruthy();
+    expect(new Food("name","brand","weight",120.0,99.99)).toBeTruthy();
   });
 });
--- frontEnd/fid-zulu/src/app/models/laptop.spec.ts
+++ frontEnd/fid-zulu/src/app/models/laptop.spec.ts
@@ -2,6 +2,6 @@ import { Laptop } from './laptop';
 
 describe('Laptop', () => {
   it('should create an instance', () => {
-    expect(new Laptop()).toBeTruthy();
+    expect(new Laptop("product","brand","CPU","memory",100.0)).toBeTruthy();
   });
 });
