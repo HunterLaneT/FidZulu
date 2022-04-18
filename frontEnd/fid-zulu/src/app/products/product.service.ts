00c94c1144d346a06f2b19493783b15d94335f0d
hunterlturner69@gmail.com <hunterlturner69@gmail.com>
2022-04-18 15:11:39 -0400

    completed dvd component

--- frontEnd/fid-zulu/src/app/products/product.service.ts
+++ frontEnd/fid-zulu/src/app/products/product.service.ts
@@ -1,47 +1,80 @@
 import { Injectable } from '@angular/core';
 import { Bike } from '../models/bike.model';
+import { Dvd } from '../models/dvd.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-
-
   mockBikes: Bike[] = [
     {
-      "name": "Mamba Sport 12\" Balance Bike",
-      "brand": "Mamba Bikes",
-      "color": "black",
-      "price": 75.88
+      name: 'Mamba Sport 12" Balance Bike',
+      brand: 'Mamba Bikes',
+      color: 'black',
+      price: 75.88,
     },
     {
-      "name": "DJ Fat Bike 500W",
-      "brand": "DJ Bikes",
-      "color": "grey",
-      "price": 1599.86
+      name: 'DJ Fat Bike 500W',
+      brand: 'DJ Bikes',
+      color: 'grey',
+      price: 1599.86,
     },
     {
-      "name": "Kobe Aluminum Balance",
-      "brand": "Kobe",
-      "color": "blue",
-      "price": 88.56
+      name: 'Kobe Aluminum Balance',
+      brand: 'Kobe',
+      color: 'blue',
+      price: 88.56,
     },
     {
-      "name": "Pomona Men's Cruiser Bike",
-      "brand": "Northwoods",
-      "color": "silver",
-      "price": 221.36
-    }
-  ]
+      name: "Pomona Men's Cruiser Bike",
+      brand: 'Northwoods',
+      color: 'silver',
+      price: 221.36,
+    },
+  ];
+
+  mockDvds: Dvd[] = [
+    {
+      title: 'Avengers - Infinity War',
+      mpaa_rating: 'PG-13',
+      studio: 'MARVEL',
+      time: 149,
+      price: 18.55,
+    },
+    {
+      title: 'Spider-Man Homecoming',
+      mpaa_rating: '14 and over',
+      studio: 'Sony Pictures Home Entertainment',
+      time: 133,
+      price: 7.23,
+    },
+    {
+      title: 'Ant-Man',
+      mpaa_rating: 'PG-13',
+      studio: 'Walt Disney Video',
+      time: 117,
+      price: 19.98,
+    },
+    {
+      title: 'Captain America',
+      mpaa_rating: 'PG',
+      studio: 'Walt Disney Video',
+      time: 123,
+      price: 24.88,
+    },
+  ];
 
   // getStocks() : Observable<Stock[]>  {
   //   return this.http.get<Stock[]>(this.url).pipe(catchError(this.handleError));
   // }
 
-  getBikes() : Bike[] {
-    return this.mockBikes
+  getBikes(): Bike[] {
+    return this.mockBikes;
   }
 
-  constructor() { }
+  getDvds(): Dvd[] {
+    return this.mockDvds;
+  }
+
+  constructor() {}
 }
