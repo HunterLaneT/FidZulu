2e664941988ddc214d4a98cf2ab5fc0f0a4c1a9e
dhruvenkumarChauhan <85200767+dhruvenkumarChauhan@users.noreply.github.com>
2022-04-18 14:39:45 -0400

    front-end change
    added component for toys

--- frontEnd/fid-zulu/src/app/app.module.ts
+++ frontEnd/fid-zulu/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BikesListComponent } from './bikes-list/bikes-list.component';
+import { ToysListComponent } from './toys-list/toys-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BikesListComponent
+    BikesListComponent,
+    ToysListComponent
   ],
   imports: [
     BrowserModule,
