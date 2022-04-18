f80ead093be6ff8dae8e47b1f7574b344639ebf3
Rahul Gawdi <rahul.gawdi@fmr.com>
2022-04-18 13:57:02 -0400

    Head start on dvds

--- backEndB/DVDs/routes/index.js
+++ backEndB/DVDs/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const createError = require('http-errors');
+const express = require('express');
+const router = express.Router();
 
+const dvds = require('../modules/dvds')
+const url = require('url');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/dvds/:location', (req, res, next)=> {
+  const raw = books.list();
+//const result = books.list();
+result.forEach(dvd => {
+  
+});
+res.setHeader('content-type', 'application/json');
+res.and(JSON.stringify(result));
 });
 
 module.exports = router;
