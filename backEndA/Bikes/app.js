ed0121313cd3d0d6c6785a0dfb74b7029bd8716e
Solomon, Jared <Jared.Solomon@fmr.com>
2022-04-18 13:54:24 -0400

    Team route implemented

--- backEndA/Bikes/app.js
+++ backEndA/Bikes/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var bikeRouter = require('./routes/bikes');
+var teamRouter = require('./routes/team');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/bikes', bikeRouter);
+app.use('/team', teamRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
