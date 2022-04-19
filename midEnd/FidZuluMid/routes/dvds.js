2109a98e9f394bd77ee119cb099095c60547fedd
a707905 <makedaphoenix@gmail.com>
2022-04-19 10:28:08 -0400

    post request for dvds and food implemented

--- midEnd/FidZuluMid/routes/dvds.js
+++ midEnd/FidZuluMid/routes/dvds.js
@@ -12,12 +12,30 @@ router.get('/:location', function(req, res, next) {
     }).catch(err => console.log(err));
 });
 
+/* Add a DVD */
+router.post('/add', function(req, res, next) {
+
+    var newDVD = req.body;
+
+    addDVDs(newDVD).then(data => {
+        res.send(data);
+    }).catch(err => res.sendStatus(err.response.status));
+});
+
 async function getDVDs(location) {
     return axios.get('http://localhost:3035/dvds/' + location)
         .then(response => {
             console.log(response.data);
             return response.data
         });
+};
+
+async function addDVDs(DVD) {
+    return axios.post('http://localhost:3034/dvds/add', DVD)
+    .then(response => {
+        console.log(response.data);
+        return response.data
+    });
 };
 
 module.exports = router;
--- midEnd/FidZuluMid/routes/food.js
+++ midEnd/FidZuluMid/routes/food.js
@@ -19,12 +19,30 @@ router.get('/teams', async function(req, res, next) {
     }).catch(err => console.log(err));
 });
 
+/* Add a Food */
+router.post('/add', function(req, res, next) {
+
+    var newFood = req.body;
+
+    addBooks(newFood).then(data => {
+        res.send(data);
+    }).catch(err => res.sendStatus(err.response.status));
+});
+
 async function getFoods(location) {
     return axios.get('http://localhost:3032/food/' + location)
         .then(response => {
             console.log(response.data);
             return response.data
         });
+};
+
+async function addFood(food) {
+    return axios.post('http://localhost:3034/books/add', food)
+    .then(response => {
+        console.log(response.data);
+        return response.data
+    });
 };
 
 module.exports = router;
