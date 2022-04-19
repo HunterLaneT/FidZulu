96ebc371e0bc310b6d4ecc08074f3200dd66223c
a707905 <makedaphoenix@gmail.com>
2022-04-19 11:27:23 -0400

    dvd spec tests

--- midEnd/FidZuluMid/spec/dvds.spec.js
+++ midEnd/FidZuluMid/spec/dvds.spec.js
@@ -89,4 +89,68 @@ describe("DVDs Mid End Tests", () => {
         });
 
     });
+
+    describe("POST /dvds/add", () => {
+        it("returns a 404 status code", (done) => {
+            nock(base_url).post(post_url).reply(404);
+
+            request.post(base_url + post_url, (err, res, body) => {
+                expect(res.statusCode).toBe(404);
+                done();
+            });
+        });
+
+        it("returns a 200 status code", (done) => {
+            let dvd = {
+                "title": "Spider-Man Homecoming",
+                "mpaa_rating": "14 and over",
+                "studio": "Sony Pictures Home Entertainment",
+                "time": 133,
+                "price": 7.23
+              };
+
+            let dvdJSON = JSON.stringify(dvd);
+
+            nock(base_url)
+                .post(post_url, dvdJSON)
+                .reply(200, {
+                    id: 123456
+                });
+
+            request.post({
+                url: base_url + post_url,
+                body: dvdJSON
+            }, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
+
+        it("returns id on successful add", (done) => {
+            let dvd = {
+                "title": "Spider-Man Homecoming",
+                "mpaa_rating": "14 and over",
+                "studio": "Sony Pictures Home Entertainment",
+                "time": 133,
+                "price": 7.23
+              };
+
+            let dvdJSON = JSON.stringify(dvd);
+
+            nock(base_url)
+                .post(post_url, dvdJSON)
+                .reply(200, {
+                    id: 123456
+                });
+            
+            request.post({
+                url: base_url + post_url,
+                body: dvdJSON
+            }, (err, res, body) => {
+                expect(body).toBeTruthy();
+                expect(body).toContain(123456);
+                done();
+            });
+        });
+    });
 });
