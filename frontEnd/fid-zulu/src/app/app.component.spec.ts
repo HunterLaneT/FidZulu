3002a6c4145e4484f0eeef3c68e9e4429a6e8c35
Micah Haycraft <micahjhaycraft@gmail.com>
2022-04-19 13:50:18 -0400

    21 passing tests

--- frontEnd/fid-zulu/src/app/app.component.spec.ts
+++ frontEnd/fid-zulu/src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Fidelity Zulu');
   });
 
-  xit('should render title', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('header')?.textContent).toContain('Fid Zulu');
+    expect(compiled.querySelector('title')?.textContent).toContain('Fidelity Zulu');
   });
 });
