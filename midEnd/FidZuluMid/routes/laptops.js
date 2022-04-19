21f3b9e3c299cf1fac876b24c0bf0d8bedab573e
Allen Wong <allenwong1234@gmail.com>
2022-04-19 10:34:58 -0400

    Add teams method and test

--- midEnd/FidZuluMid/routes/laptops.js
+++ midEnd/FidZuluMid/routes/laptops.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const request = require('request');
+const axios = require('axios');
 var router = express.Router();
 
 /* Calculate Laptop Prices */
@@ -12,8 +12,22 @@ router.get('/:location', function(req, res, next) {
     }).catch(err => console.log(err));
 });
 
+router.get('/teams', async function(req, res, next) {
+    getTeams().then(data => {
+        res.send(data);
+    }).catch(err => console.log(err));
+});
+
 async function getLaptops(location) {
     return axios.get('http://localhost:3036/laptops/' + location)
+        .then(response => {
+            console.log(response.data);
+            return response.data
+        });
+};
+
+async function getTeams() {
+    return axios.get('http://localhost:3036/laptops/teams')
         .then(response => {
             console.log(response.data);
             return response.data
