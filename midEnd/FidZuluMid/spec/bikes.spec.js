10dcc026c0793c1ff9daa2579de83df787d9ce84
Hanna, Waseem <wn.said0@gmail.com>
2022-04-19 11:22:06 -0400

    finished bikes POST spec tests

--- midEnd/FidZuluMid/spec/bikes.spec.js
+++ midEnd/FidZuluMid/spec/bikes.spec.js
@@ -2,6 +2,7 @@ const request = require("request");
 const nock = require("nock");
 
 const base_url = 'http://localhost:3021';
+const post_url = '/bikes/add';
 
 describe("Bikes Mid End Tests", () => {
     describe("GET /bikes/<location>", () => {
@@ -49,6 +50,68 @@ describe("Bikes Mid End Tests", () => {
             request.get(base_url + location, (err, res, body) => {
                 expect(body).toBeTruthy();
                 expect(body).toContain("Mamba Bikes");
+                done();
+            });
+        });
+    });
+
+    describe("POST /bikes/add", () => {
+        it("returns a 404 status code", (done) => {
+            nock(base_url).post(post_url).reply(404);
+
+            request.post(base_url + post_url, (err, res, body) => {
+                expect(res.statusCode).toBe(404);
+                done();
+            });
+        });
+
+        it("returns a 200 status code", (done) => {
+            let bike = {
+                "name": "Mamba Sport 12\" Balance Bike",
+                "brand": "Mamba Bikes",
+                "color": "black",
+                "price": 75.88
+            };
+
+            let bikeJSON = JSON.stringify(bike);
+
+            nock(base_url)
+                .post(post_url, bikeJSON)
+                .reply(200, {
+                    id: 123456
+                });
+
+            request.post({
+                url: base_url + post_url,
+                body: bikeJSON
+            }, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
+
+        it("returns id on successful add", (done) => {
+            let bike = {
+                "name": "Mamba Sport 12\" Balance Bike",
+                "brand": "Mamba Bikes",
+                "color": "black",
+                "price": 75.88
+            };
+
+            let bikeJSON = JSON.stringify(bike);
+
+            nock(base_url)
+                .post(post_url, bikeJSON)
+                .reply(200, {
+                    id: 123456
+                });
+            
+            request.post({
+                url: base_url + post_url,
+                body: bikeJSON
+            }, (err, res, body) => {
+                expect(body).toBeTruthy();
+                expect(body).toContain(123456);
                 done();
             });
         });
